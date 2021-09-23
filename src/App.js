9acdc1e081cb64b516017b1339015a827669815f
mikyGithub <54312715+mikyGithub@users.noreply.github.com>
2021-09-23 12:07:14 -0700

    'tailwind-added'

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import React from 'react';
-import './App.css';
-import Header from './components/Header';
+import React from 'react'
+import Header from './components/Header'
 
 function App() {
   return (
-    <div className="App">
-            <Header></Header>
+    <div className='container mx-auto flex items-center flex-col'>
+      {/* Header */}
+      <Header/>
+      {/* App Body */}
+      <div className='flex'>
+          <div className='w-2/12'>
+{/* Sidebar */}
+Sidebar
+          </div>
+          <div className='w-8/12'>
+{/* Main Content */}
+Main Content
+          </div>
+          
+          <div className='w-2/12'>
+              <div>
+                Feeds
+              </div>
+          </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
- 
+export default App
